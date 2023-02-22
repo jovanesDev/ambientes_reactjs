b0276fc72c988c1dc41b5e22b890730613077040
Hovha <jovanes.web@gmail.com>
2023-02-22 19:43:45 -0300

    created production env

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  // ambiente developmente
+  // ambiente produccion
   return (
     <div className="App">
       <header className="App-header">
